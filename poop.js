f60f1c0d4c382df7738cb0664299b2f011436e7d
Tejas Anand <tejasanand@tejass-macbook-pro.home>
2021-05-28 21:49:46 +0200

    Initial commit

---
+++ poop.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".nav-menu");
+const navLogo = document.querySelector("#nav-logo");
+const body = document.querySelector("body");
+
+
+const mobileMenu = () =>{
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+    body.classList.toggle("active");
+}
+
+menu.addEventListener("click", mobileMenu);
+
+
+gsap.registerPlugin(ScrollTrigger);
